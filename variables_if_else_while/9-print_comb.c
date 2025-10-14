a2ac8fc5c603f942e57376b5ab616b30918aba38
Lachie King <kingy00112233@gmail.com>
2025-10-14 12:18:08 +0000

    update 9-print_comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-int i, j;
-for (i = 0; i <= 8; i++)
-{
-for (j = i + 1; j <= 9; j++)
+int i;
+for (i = 0; i <= 9; i++)
 {
 putchar(i + '0');
-putchar(j + '0');
-putchar((i != 8 || j != 9) ? ',' : '\n');
-putchar((i != 8 || j != 9) ? ' ' : 0);
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
 }
 }
+putchar('\n');
 return (0);
 }
