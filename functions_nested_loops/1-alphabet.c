190fce1ad0b3e30622a7dfdf74ddc77b23f6701f
Lachie King <kingy00112233@gmail.com>
2025-10-15 01:38:32 +0000

    update 1-alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,10 @@
 #include "main.h"
+#include "_putchar.c"
 
-/**
- * main - Entry Point
- * Return: Always 0
- */
-
-int main(void)
+void print_alphabet(void)
 {
-print_alphabet();
-return (0);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n')
 }
