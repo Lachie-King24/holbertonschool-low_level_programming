fc9135fd2b77c39ec6084276ca8c361a57a0664a
Lachie King <kingy00112233@gmail.com>
2025-10-15 05:49:19 +0000

    update 7-print_last_digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -13,10 +13,12 @@ int last_digit;
 last_digit = r % 10;
 if (last_digit < 0)
 {
+_putchar(last_digit + '0');
 return (-last_digit);
 }
 else
 {
+_putchar(last_digit + '0');
 return (last_digit);
 }
 }
