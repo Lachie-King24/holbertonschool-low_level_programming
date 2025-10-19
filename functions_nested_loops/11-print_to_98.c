a584f45b9578370954ee1363ef72e5b5dad4db7c
Lachie King <kingy00112233@gmail.com>
2025-10-19 11:25:31 +0000

    update 11-print_to_98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,44 +1,22 @@
 #include "main.h"
 
 /**
- * print_to_98 - print numbers. n-98
- * @n: num to count from
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting number
  */
-
 void print_to_98(int n)
 {
 int i;
-if (n <= 98)
-{
-for (i = n; i <= 98; i++)
+for (i = n; i != 98; i += (n < 98 ? 1 : -1))
 {
 if (i < 0)
 {
 _putchar('-');
 if (-i >= 10)
-{
 _putchar((-i / 10) + '0');
-}
 _putchar((-i % 10) + '0');
 }
-else
-{
-if (i >= 10)
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
-}
-if (i != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-}
-else
-{
-for (i = n; i >= 98; i--)
-{
-if (i >= 10)
+else if (i >= 100)
 {
 _putchar((i / 100) + '0');
 _putchar(((i / 10) % 10) + '0');
@@ -51,12 +29,10 @@ _putchar((i % 10) + '0');
 }
 else
 _putchar(i + '0');
-if (i != 98)
-{
 _putchar(',');
 _putchar(' ');
 }
-}
-}
+_putchar('9');
+_putchar('8');
 _putchar('\n');
 }
