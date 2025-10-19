33187804c767467a8f1857f09f858d8c537e3ff7
Lachie King <kingy00112233@gmail.com>
2025-10-19 11:31:58 +0000

    update 11-print_to_98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,29 +6,28 @@
  */
 void print_to_98(int n)
 {
-int i;
+  int i, num;
 for (i = n; i != 98; i += (n < 98 ? 1 : -1))
 {
-if (i < 0)
+num = i;
+if (num < 0)
 {
 _putchar('-');
-if (-i >= 10)
-_putchar((-i / 10) + '0');
-_putchar((-i % 10) + '0');
+ num = -num;
 }
-else if (i >= 100)
+if (num >= 100)
 {
-_putchar((i / 100) + '0');
-_putchar(((i / 10) % 10) + '0');
-_putchar((i % 10) + '0');
+_putchar((num / 100) + '0');
+ _putchar(((num / 10) % 10) + '0');
+_putchar((num % 10) + '0');
 }
-else if (i >= 10)
+else if (num >= 10)
 {
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
+_putchar((num / 10) + '0');
+_putchar((num % 10) + '0');
 }
 else
-_putchar(i + '0');
+_putchar(num + '0');
 _putchar(',');
 _putchar(' ');
 }
