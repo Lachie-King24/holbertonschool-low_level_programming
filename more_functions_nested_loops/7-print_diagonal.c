885d1541ce84d6d28d88dc6ec8b6cf2e7c8c70b4
Lachie King <kingy00112233@gmail.com>
2025-10-21 01:43:22 +0000

    update 7-print_diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,14 @@
 
 void print_diagonal(int n)
 {
-  int d;
-  if (n <= 0)
-    {
-    _putchar('\n');
-    }
-  for (d = 0; d < n; d++)
-    {
-      _putchar('\\');
-    }
-  _putchar('\n')
+int d;
+if (n <= 0)
+{
+_putchar('\n');
+}
+for (d = 0; d < n; d++)
+{
+_putchar('\\');
+}
+_putchar('\n');
 }
