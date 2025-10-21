7e4fff7969d75939ab8c7d9f3f30010b6811733a
Lachie King <kingy00112233@gmail.com>
2025-10-21 01:41:28 +0000

    add 7-print_diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a line diagonally
+ *
+ * @n - number of lines
+ */
+
+void print_diagonal(int n)
+{
+  int d;
+  if (n <= 0)
+    {
+    _putchar('\n');
+    }
+  for (d = 0; d < n; d++)
+    {
+      _putchar('\\');
+    }
+  _putchar('\n')
+}
