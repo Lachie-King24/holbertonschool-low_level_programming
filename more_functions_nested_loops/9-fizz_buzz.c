235694ffec766318fc1ed48418d3fc7cf2ae62bc
Lachie King <kingy00112233@gmail.com>
2025-10-21 03:20:27 +0000

    update 9-fizz_buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int a;
+for (a = 1; a <= 100; a++)
+{
+if ((a % 3) == 0)
+{
+printf("Fizz");
+}
+else if ((a % 5) == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", a);
+}
+printf(" ");
+}
+printf("\n");
+}
