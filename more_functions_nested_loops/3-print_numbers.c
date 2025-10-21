fb636e2492d01f063146f8326909ece2362f3577
Lachie King <kingy00112233@gmail.com>
2025-10-21 00:29:57 +0000

    add 3-print_numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0-9 followed by a new line
+ *
+ * Return: result of print_numbers
+ */
+
+void print_numbers(void)
+{
+int a;
+for (a = '0', a <= 9, a++)
+{
+_putchar(print_numbers)
+}
+_putchar('\n');
+}
