8e8fcf32ccf2c5b58c75ea2d1372e6a3f3d04f42
Lachie King <kingy00112233@gmail.com>
2025-10-23 12:28:38 +0000

    update 6-puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+int length(char *str)
+{
+	int i;
+	i = 0;
+	while (str[i] != '\0')
+	  {
+	    i++;
+	  }
+	return (i);
+}
+
 /**
  * puts2 - print 2nd char of string
  * @str: string
@@ -10,8 +21,9 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-
-	while (str[i] != '\0')
+	int len = length(str);
+	
+	while (i < len)
 	{
 	_putchar(str[i]);
 	i += 2;
