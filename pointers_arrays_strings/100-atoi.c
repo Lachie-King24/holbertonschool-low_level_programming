1bbcc56ba6aace6c70040cf2709ad37a4cae02e4
Lachie King <kingy00112233@gmail.com>
2025-10-24 05:24:40 +0000

    Add 100-atoi and update main

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an int
+ * @s: string to convert
+ * Return: integer found in string
+ */
+
+int _atoi(char *s)
+{
+	int i, num = 0;
+	int negative = 0;
+	int found_digit = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-' && s[i + 1] >= '0' && s[i + 1] <= '9')
+		{
+			negative = 1;
+		}
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+		num = num * 10 + (s[i] - '0');
+		found_digit = 1;
+		}
+		else
+		{
+		if (found_digit)
+		break;
+		}
+}
+
+if (negative)
+num = -num;
+
+return (num);
+}
