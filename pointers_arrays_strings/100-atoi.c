9c3ac3a19945c8529f176c9a380f7df79d15e4f8
Lachie King <kingy00112233@gmail.com>
2025-10-24 05:43:50 +0000

    Add 100-atoi and update main

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,30 +9,39 @@
 
 int _atoi(char *s)
 {
-    int i, num = 0;
-    int negative = 0;
-    int found_digit = 0;
+	int i, num = 0;
+	int negative = 0;
+	int found_digit = 0;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        if (s[i] == '-' && !found_digit)
-        {
-            negative = !negative;
-        }
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            num = num * 10 + (s[i] - '0');
-            found_digit = 1;
-        }
-        else
-        {
-            if (found_digit)
-                break;
-        }
-    }
+	for (i = 0; s[i] != '\0'; i++)
+	  {
 
-    if (negative)
-        num = -num;
+	if (s[i] == '-' && !found_digit)
+	{
+	negative = !negative;
+	}
 
-    return num;
+	else if (s[i] >= '0' && s[i] <= '9')
+	{
+		int digit = s[i] - '0';
+
+		if (!negative && num > (INT_MAX - digit) / 10)
+		return INT_MAX;
+		if (negative && num > (INT_MAX - digit) / 10)
+		return INT_MIN;
+
+		num = num * 10 + digit;
+		found_digit = 1;
+	}
+	else
+	{
+		if (found_digit)
+		break;
+	}
+	}
+
+	if (negative)
+	num = -num;
+
+	return num;
 }
