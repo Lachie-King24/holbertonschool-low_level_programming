cd3094508ff8164083a5961450ecb806eea3fb4c
Lachie King <kingy00112233@gmail.com>
2025-10-26 03:39:25 +0000

    add 5-string_toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @s: string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
