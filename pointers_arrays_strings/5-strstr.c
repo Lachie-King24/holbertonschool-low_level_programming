cdd1aeadf20bc6dea7a615742170dea0767e805c
Lachie King <kingy00112233@gmail.com>
2025-10-28 02:33:51 +0000

    update 5-strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,12 +11,27 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
+	int j;
 
+	if (*needle == '\0')
+		return (haystack);
+	
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (haystack[i] == needle[0])
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			return (haystack + i);
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+		    return (haystack + i);
 		}
 	}
 	return (NULL);
