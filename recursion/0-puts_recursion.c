bd8589df4a5cf4a748035e5ac3def241dce2c9c0
Lachie King <kingy00112233@gmail.com>
2025-10-29 02:38:52 +0000

    add 0-puts_recursion and add main

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts_recursion - prints a string
+ * @s: pointer to a character
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	  	_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
