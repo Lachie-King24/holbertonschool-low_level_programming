3561a9d51f73d87ab29bfd8d33c41935f3c0bf2c
Lachie King <kingy00112233@gmail.com>
2025-10-30 02:11:18 +0000

    update main and add 5-sqrt_recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * workOut - calculates multiples of x
+ * _sqrt_recursion - uses workOut to match the value of x to be the square root of 9
+ * @n: number entered
+ * @x: square root of number
+ * Return: square root of n
+ */
+
+int workOut(int n, int x)
+{
+	if (x * x == n)
+	{
+	  return (x);
+	}
+	if (x * x > n)
+	{
+	return (-1);
+	}
+	return (workOut(n, x + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+	return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+	return (n);
+	}
+	return (workOut(n, 1));
+}
