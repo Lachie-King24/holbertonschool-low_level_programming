38ed8705a56fd9ca1a54eea76251a52800f99f3c
Lachie King <kingy00112233@gmail.com>
2025-10-31 04:46:45 +0000

    update 1-args

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - print number of arguments
+ * @argc: argument count
+ * @argv: argument array
+ * Return: always 0
+ */
+
 int main(int argc, char *argv[])
 {
 	(void)argv;
