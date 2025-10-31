36460be2c9421ad8a13b06f939fbf7c5f22784f7
Lachie King <kingy00112233@gmail.com>
2025-10-31 04:56:10 +0000

    add 2-args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print all arguments
+ * @argc: argument count
+ * @argv: argument array
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; argc > i; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
