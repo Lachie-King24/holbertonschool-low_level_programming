67250041f90dcb1d05d0d7dd7dedf3f8cceae0bb
Lachie King <kingy00112233@gmail.com>
2025-10-31 05:16:38 +0000

    add 3-mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies 2 numbers
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0 if 2 digits entered. otherwise 1.
+ */
+
+int main(int argc, char *argv[])
+{
+	int num;
+	int num2;
+	int result;
+	
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num * num2;
+	printf("%d\n", result);
+	return (0);
+}
