909f01b1707a7c9b0a241e958735ca84345a6ae1
Lachie King <kingy00112233@gmail.com>
2025-11-05 01:26:35 +0000

    add 1-strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *_strdup(char *str);
+{
+	char *dup;
+	unsigned int i;
+	unsigned int len;
+
+	len = 0;
+  
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (len != '\0')
+	{
+		len++
+		  }
+
+	dup = malloc((len + 1) * sizeof(char));
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= len; i++)
+	{
+		dup[i] = str[i];
+	}
+
+	return (dup);
+}
