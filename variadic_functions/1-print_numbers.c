f339905139e8a90b0cb119ce3c5f16a242dce857
Lachie King <kingy00112233@gmail.com>
2025-11-13 03:54:57 +0000

    add 1-print_numbers and update variadic_functions

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers, seperated by separator
+ * @separator: separates numbers
+ * @n: number of arg
+ * Return: Void/Nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	unsigned int num;
+
+	va_start(ap, n);
+
+	if (separator == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(ap, int);
+		printf("%d", num);
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
