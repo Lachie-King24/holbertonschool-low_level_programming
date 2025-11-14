479b816c74a2cf1be7e5a91c6821fe3cb0f2d1b9
Lachie King <kingy00112233@gmail.com>
2025-11-14 01:16:48 +0000

    update 3-print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,9 +1,8 @@
 #include "variadic_functions.h"
 
 /**
- * 
- *
- *
+ * print_a_char - prints a character
+ * @args: argument in va_list
  */
 
 void print_a_char(va_list args)
@@ -11,23 +10,46 @@ void print_a_char(va_list args)
 	printf("%c", va_arg(args, int));
 }
 
+/**
+ * print_an_int - prints an int
+ * @args: argument in va_list
+ */
+
 void print_an_int(va_list args)
 {
 	printf("%d", va_arg(args, int));
 }
+
+/**
+ * print_a_float - prints a float
+ * @args: argument in va_list
+ */
 
 void print_a_float(va_list args)
 {
 	printf("%f", va_arg(args, double));
 }
 
+/**
+ * print_a_string - prints a string
+ * @args: argument in va_list
+ * if !s (NULL), print nil
+ */
+
 void print_a_string(va_list args)
 {
 	char *s = va_arg(args, char *);
+
 	if (!s)
 		s = "(nil)";
 	printf("%s", s);
 }
+
+/**
+ * print_all - print any character type
+ * @format: character type
+ * Return: Nothing
+ */
 
 void print_all(const char * const format, ...)
 {
