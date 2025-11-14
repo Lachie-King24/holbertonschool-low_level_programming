552a101226cf0e9685909c188e0f41943d979788
Lachie King <kingy00112233@gmail.com>
2025-11-14 01:12:04 +0000

    add 3-print_all and update variadic_functions

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -6,27 +6,63 @@
  *
  */
 
+void print_a_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+void print_an_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+void print_a_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+void print_a_string(va_list args)
+{
+	char *s = va_arg(args, char *);
+	if (!s)
+		s = "(nil)";
+	printf("%s", s);
+}
+
 void print_all(const char * const format, ...)
 {
-	va_list ap;
-	unsigned int i;
-	unsigned int j;
-	const char arg[] = "cifs";
+	va_list args;
+	int i;
+	int j;
+	char *sep = "";
+	form_t ops[] = {
+		{"c", print_a_char},
+		{"i", print_an_int},
+		{"f", print_a_float},
+		{"s", print_a_string},
+		{NULL, NULL}
+	};
 
 	i = 0;
 
-	va_start(ap, format);
+	va_start(args, format);
 
 	while (format && format[i])
 	{
 		j = 0;
-
-		while (arg[j])
+		while (ops[j].symbol != NULL)
 		{
-			if (format[i] == arg[j])
-				printf(" ")
+			if (format[i] == ops[j].symbol[0])
+			{
+				printf("%s", sep);
+				ops[j].print(args);
+				sep = ", ";
+				break;
+			}
+			j++;
 		}
-
-
+		i++;
 	}
+	printf("\n");
+	va_end(args);
 }
