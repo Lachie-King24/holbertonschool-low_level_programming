5f6e2bd06026421651f82fa77bbabee70b3f6ca4
Lachie King <kingy00112233@gmail.com>
2025-11-25 02:48:36 +0000

    fix compiler issue

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -27,7 +27,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	for (len = 0; str[len] != '\0'; len++;)
+	for (len = 0; str[len] != '\0'; len++)
 		;
 	carrier->len = len;
 
