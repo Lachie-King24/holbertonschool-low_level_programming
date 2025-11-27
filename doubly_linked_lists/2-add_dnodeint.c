5129e94221b1db7ea853e1b1a95db80e92b01a9d
Lachie King <kingy00112233@gmail.com>
2025-11-27 04:41:17 +0000

    add documents for task 3

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add a new node at beginning of list
+ * @n: node within list
+ * Return: address of new node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *carrier;
+	
+	if (head == NULL)
+		return (NULL);
+
+	carrier = malloc(sizeof(dlistint_t));
+	if (carrier == NULL)
+		return (NULL);
+
+	carrier->n = n;
+	carrier->prev = NULL;
+	carrier->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = carrier;
+
+	*head = carrier;
+
+	return (carrier);
+}
