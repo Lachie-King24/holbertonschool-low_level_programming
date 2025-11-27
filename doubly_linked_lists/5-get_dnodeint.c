e730ba06942703c8e35e9279beefee5b5ff45d77
Lachie King <kingy00112233@gmail.com>
2025-11-27 05:40:58 +0000

    add documents for task 6

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - return the nth node of the list
+ * @head: head of the list
+ * @index: index of node in list
+ * Return: node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count;
+	dlistint_t *temp;
+
+	count = 0;
+	*temp = head;
+
+	while (temp != NULL)
+	{
+		if (count == index)
+			return (temp);
+
+		temp = temp->next;
+		count++;
+	}
+	return (NULL);
+}
