e0b295dd0d3406a9e14a97ab23b8b2b4bf2d781f
Lachie King <kingy00112233@gmail.com>
2025-11-28 03:40:17 +0000

    add documents for task 7

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "links.h"
+
+/**
+ * sum_dlistint - returns the sum of all data
+ * @head: head node of the list
+ * Return: sum of data in all nodes
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
