b6012d9a5fdc5ab12dfbb813b391a593a578292d
Lachie King <kingy00112233@gmail.com>
2025-12-03 04:08:17 +0000

    Add documents for task 4 and update header file

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_table.c"
+
+/**
+ * hash_table_get - gets a value associated with a key
+ * @ht: pointer to the hash table
+ * @key: pointer to the key
+ * Return: value or NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (!ht || !key || !*key)
+		return NULL;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
