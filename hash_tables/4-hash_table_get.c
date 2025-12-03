e829207c09be0157535d75c4cd14927b842f832e
Lachie King <kingy00112233@gmail.com>
2025-12-03 04:11:25 +0000

    update a return statement

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *temp;
 
 	if (!ht || !key || !*key)
-		return NULL;
+		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
